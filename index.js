fd8f4998dab434ac537e1dde6d40e3e6c777372b
Monalisa Onyango <+>
2024-07-16 18:02:14 +0300

    hmmmm

--- index.js
+++ index.js
@@ -1,6 +1,63 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+     
+    // const seen = new Set();
+    // for (let num of array) {
+    //     const complement = target - num;
+    //     if (seen.has(complement)) {
+    //         return true;
+    //     }
+    //     seen.add(num);
+    // }
+    // return false;
+// ...............................................................................
+    // for (let i = 0; i < array.length; i++) {
+
+    //       const complement = target - array[i];
+
+    //       for (let j = i + 1; j < array.length; j++) {
+    //         if (array[j] === complement) return true;
+    //       }
+    //     }
+      
+    //     return false;
+// ..............................................................................
+
+// // iterate over the array of numbers
+// for (let i = 0; i < array.length; i++) {
+//   // for the current number, identify a complementary number that adds to our target
+//   // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+//   const complement = target - array[i];
+//   // iterate over the remaining numbers in the array
+//   for (let j = i + 1; j < array.length; j++) {
+//     // check if any of the remaining numbers is the complement
+//     // if so, return true
+//     if (array[j] === complement) return true;
+//   }
+// }
+// // if we reach the end of the array, return false
+// return false;
+
+//................................................................................................
+
+  for (let i = 0; i < array.length; i++) {
+    // n steps (depending on the length of the input array)
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      // n * n steps (nested loop!)
+      if (array[j] === complement) return true;
+    }
+  }
+  // 1 step
+  return false;
+
 }
+ 
+
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,7 +65,17 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+
+      come with new array
+
+      iterates each element of the array
+
+
+
+      return true
+
+*/   
+    
 
 /*
   Add written explanation of your solution here
